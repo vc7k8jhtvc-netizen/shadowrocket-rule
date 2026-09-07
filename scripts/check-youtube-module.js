4dc4dd60a061b4e3c67e5f4ab94254f211bb369f
vc7k8jhtvc-netizen <vc7k8jhtvc@privaterelay.appleid.com>
2026-09-08 00:12:49 +0800

    Point YouTube checks to Shadowrocket routing config

--- scripts/check-youtube-module.js
+++ scripts/check-youtube-module.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const root = path.resolve(__dirname, '..');
 const text = fs.readFileSync(path.join(root, 'YouTube.Enhance.Shadowrocket.sgmodule'), 'utf8');
-const config = fs.readFileSync(path.join(root, 'Shadowrocket_Standalone_v2.6.5.conf'), 'utf8');
+const config = fs.readFileSync(path.join(root, 'Shadowrocket_Routing_v2.7.0.conf'), 'utf8');
 const assert = (ok, message) => { if (!ok) throw new Error(message); };
 function section(name) {
   const start = text.indexOf('[' + name + ']');
