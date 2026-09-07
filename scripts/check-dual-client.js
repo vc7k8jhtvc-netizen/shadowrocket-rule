87baf76ef5b77b04dc5ac1d9c5fa6efb5795c83a
vc7k8jhtvc-netizen <vc7k8jhtvc@privaterelay.appleid.com>
2026-09-08 00:02:48 +0800

    Use v2.7 routing config for dual-client parity

--- scripts/check-dual-client.js
+++ scripts/check-dual-client.js
@@ -3,7 +3,7 @@ const path = require('path');
 const vm = require('vm');
 
 const root = path.resolve(__dirname, '..');
-const shadowrocketPath = path.join(root, 'Shadowrocket_Standalone_v2.6.5.conf');
+const shadowrocketPath = path.join(root, 'Shadowrocket_Routing_v2.7.0.conf');
 const clashPath = path.join(root, 'Clash_Verge_Rev_Script.js');
 
 const shadowrocket = fs.readFileSync(shadowrocketPath, 'utf8');
