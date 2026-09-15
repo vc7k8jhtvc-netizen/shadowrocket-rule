b03bd2c74a81bb798a223e031085d15262ee0017
vc7k8jhtvc-netizen <vc7k8jhtvc@privaterelay.appleid.com>
2026-09-16 00:29:09 +0800

    test: update merged module version fixture

--- scripts/check-validator-regressions.js
+++ scripts/check-validator-regressions.js
@@ -54,13 +54,13 @@ try{
 
   const readmePath=path.join(temp,'README.md');
   const readme=fs.readFileSync(readmePath,'utf8');
-  fs.writeFileSync(readmePath,readme.replace('内部版本为 `v2.7.16`','内部版本为 `v9.9.9`'));
+  fs.writeFileSync(readmePath,readme.replace('内部版本为 `v2.7.17`','内部版本为 `v9.9.9`'));
   check('check-version.js',[],1,'version mismatch');
   fs.writeFileSync(readmePath,readme);
 
   const changelogPath=path.join(temp,'CHANGELOG.md');
   const changelog=fs.readFileSync(changelogPath,'utf8');
-  fs.writeFileSync(changelogPath,changelog.replace('内部版本升至 `v2.7.16`','内部版本升至 `v9.9.9`'));
+  fs.writeFileSync(changelogPath,changelog.replace('内部版本升至 `v2.7.17`','内部版本升至 `v9.9.9`'));
   check('check-version.js',[],1,'version mismatch');
   fs.writeFileSync(changelogPath,changelog);
 
